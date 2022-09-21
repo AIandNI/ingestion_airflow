6dcf23d488e211ba40ab0f0440c60d36ecc3309d
Ashok <ashok@Ashoks-MacBook-Air.local>
2022-09-20 20:10:07 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import sys
+from config import DB_DETAILS
+def main():
+    #'''Program takes at least one argument'''
+    env = sys.argv[1]
+    print(env)
+    dbdetails = DB_DETAILS[env]
+    #print(dbdetails)
+
+
+if __name__ == '__main__':
+    main()
